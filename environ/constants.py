1408859bd5874426fedbb73701a1c62661f1bc38
Yichen Luo <103887158+lyc0603@users.noreply.github.com>
2023-10-04 09:01:59 +0100

    directory setting

---
+++ environ/constants.py
@@ -0,0 +1,12 @@
+"""
+Constants for the project
+"""
+
+from pathlib import Path
+
+from environ.settings import PROJECT_ROOT
+
+# Path to the directory
+DATA_PATH: Path = PROJECT_ROOT / "data"
+PROCESSED_DATA_PATH: Path = DATA_PATH / "processed"
+TABLE_PATH: Path = DATA_PATH / "tables"
