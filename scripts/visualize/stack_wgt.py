65dfab8359aff01a61d6a8b5097d22c71fe0aa2e
Yichen Luo <103887158+lyc0603@users.noreply.github.com>
2023-11-21 15:13:47 +0000

    readjust the figure size for weight

--- scripts/visualize/stack_wgt.py
+++ scripts/visualize/stack_wgt.py
@@ -8,7 +8,7 @@ from environ.constants import FIGURE_PATH
 from scripts.process.preprocess_wgt import PLOT_DICT
 
 cmap = plt.get_cmap("tab20")
-
+FONT_SIZE = 15
 
 for period, period_info in PLOT_DICT.items():
     for strategy, strategy_wgt in period_info.items():
@@ -18,7 +18,13 @@ for period, period_info in PLOT_DICT.items():
         # plot the stake bar chart using the token-color mapping
         df_wgt_pivot.plot.bar(stacked=True, colormap=cmap, figsize=(12, 10))
 
-        plt.legend()
+        # let the legend be left outside the plot and large font
+        plt.legend(loc="center left", bbox_to_anchor=(1, 0.5), fontsize=FONT_SIZE)
+
+        # let x ticks and y ticks be large font
+        plt.xticks(fontsize=FONT_SIZE)
+        plt.yticks(fontsize=FONT_SIZE)
+
         plt.tight_layout()
         plt.savefig(
             FIGURE_PATH / f"{strategy}_{period}_wgt.pdf".replace(" ", "_"), dpi=300
