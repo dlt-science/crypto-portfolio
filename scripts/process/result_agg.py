84b7975c91df5d96d0ba4740d7323484a4e601f8
Yichen Luo <103887158+lyc0603@users.noreply.github.com>
2023-11-27 13:43:42 +0000

    update all data

--- scripts/process/result_agg.py
+++ scripts/process/result_agg.py
@@ -6,7 +6,7 @@ from environ.process.optimization import freq_iterate
 from scripts.process.preprocess_crypto_panel import df_crypto_processed
 
 # a dict to store the result
-dict_result = freq_iterate(df_crypto_processed, cash_con="0.33")
+dict_result = freq_iterate(df_crypto_processed, cash_con="0.1")
 
 # add the benchmark
 dict_result_with_benchmark = dict_result.copy()
