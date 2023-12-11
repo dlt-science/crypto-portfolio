5fba6bb4ac5d6f51d7ce79938d669953c4f18b22
Yichen Luo <103887158+lyc0603@users.noreply.github.com>
2023-12-11 16:07:04 +0000

    update wealth

--- scripts/visualize/wealth.py
+++ scripts/visualize/wealth.py
@@ -11,33 +11,40 @@ from scripts.process.result_agg import dict_result_with_benchmark
 # plot the cumulative return
 plt.figure(figsize=(12, 8))
 
-# plot the boom bust periods and legend
-for i in BOOM_BUST:
-    if i["main_trend"] == "boom":
-        plt.axvspan(i["start"], i["end"], facecolor="g", alpha=0.2)
-    if i["main_trend"] == "bust":
-        plt.axvspan(i["start"], i["end"], facecolor="r", alpha=0.2)
-    if i["main_trend"] == "none":
-        plt.axvspan(i["start"], i["end"], facecolor="grey", alpha=0.2)
-
-# label the legend for boom bust periods
-plt.axvspan(
-    BOOM_BUST[0]["start"], BOOM_BUST[0]["start"], facecolor="g", alpha=0.2, label="Boom"
-)
-plt.axvspan(
-    BOOM_BUST[0]["start"], BOOM_BUST[0]["start"], facecolor="r", alpha=0.2, label="Bust"
-)
-plt.axvspan(
-    BOOM_BUST[0]["start"],
-    BOOM_BUST[0]["start"],
-    facecolor="grey",
-    alpha=0.2,
-    label="None",
-)
-
 # plot the strategy
 for cash_con in ["0.33", "0.1"]:
     for freq in ["weekly", "montly", "quarterly"]:
+        # plot the boom bust periods and legend
+        for i in BOOM_BUST:
+            if i["main_trend"] == "boom":
+                plt.axvspan(i["start"], i["end"], facecolor="g", alpha=0.2)
+            if i["main_trend"] == "bust":
+                plt.axvspan(i["start"], i["end"], facecolor="r", alpha=0.2)
+            if i["main_trend"] == "none":
+                plt.axvspan(i["start"], i["end"], facecolor="grey", alpha=0.2)
+
+        # label the legend for boom bust periods
+        plt.axvspan(
+            BOOM_BUST[0]["start"],
+            BOOM_BUST[0]["start"],
+            facecolor="g",
+            alpha=0.2,
+            label="Boom",
+        )
+        plt.axvspan(
+            BOOM_BUST[0]["start"],
+            BOOM_BUST[0]["start"],
+            facecolor="r",
+            alpha=0.2,
+            label="Bust",
+        )
+        plt.axvspan(
+            BOOM_BUST[0]["start"],
+            BOOM_BUST[0]["start"],
+            facecolor="grey",
+            alpha=0.2,
+            label="None",
+        )
         dict_result = dict_result_with_benchmark[cash_con][freq].copy()
         for strategy, strategy_info in dict_result.items():
             plt.plot(
